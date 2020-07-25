35965118e78b023422de2fd8051396ec9eb2d560
Cyn-D <cynthia.l.douglas@gmail.com>
2020-07-24 20:23:04 -0400

    Made even more progress
    working on game logic...

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -8,7 +8,7 @@ const newGameSuccess = function (response) {
     return store.counter
   }
   numOfGame(response.game)
-  // This allows me to start a new game but all boxes need to have been clicked.
+  // Need to make it conditional that all cells are empty
   if ($('.cell').is(':empty')) {
     $('#gamearea').show()
     $('#start-game').hide()
@@ -36,48 +36,25 @@ const clickSuccess = function (response) {
   $(store.currentBox).text(store.player)
   $('#message').text(`You played ${store.player}`)
   $('#start-new-game').show()
-  // if (store.player === 'X') {
-  //   if (store.player === 'X' && response.game.cells[0] && response.game.cells[1] && response.game.cells[2] === 'X') {
-  //     if (store.player === 'X' && response.game.cells[3] && response.game.cells[4] && response.game.cells[5] === 'X') {
-  //       if (store.player === 'X' && response.game.cells[6] && response.game.cells[7] && response.game.cells[8] === 'X') {
-  //         if (store.player === 'X' && response.game.cells[0] && response.game.cells[3] && response.game.cells[6] === 'X') {
-  //           if (store.player === 'X' && response.game.cells[1] && response.game.cells[4] && response.game.cells[7] === 'X') {
-  //             if (store.player === 'X' && response.game.cells[2] && response.game.cells[5] && response.game.cells[8] === 'X') {
-  //               if (store.player === 'X' && response.game.cells[0] && response.game.cells[4] && response.game.cells[8] === 'X') {
-  //                 if (store.player === 'X' && response.game.cells[2] && response.game.cells[4] && response.game.cells[6] === 'X') {
-  //                 }
-                  // $('#message').text('Yay! X wins')
-                  // // Here I need to freeze the board to no longer allow clicks
-                  // $('.cell').off('click')
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // } else if (store.player === 'O') {
-  //   if (store.player === 'O' && response.game.cells[0] && response.game.cells[1] && response.game.cells[2] === 'O') {
-  //     if (store.player === 'O' && response.game.cells[3] && response.game.cells[4] && response.game.cells[5] === 'O') {
-  //       if (store.player === 'O' && response.game.cells[6] && response.game.cells[7] && response.game.cells[8] === 'O') {
-  //         if (store.player === 'O' && response.game.cells[0] && response.game.cells[3] && response.game.cells[6] === 'O') {
-  //           if (store.player === 'O' && response.game.cells[1] && response.game.cells[4] && response.game.cells[7] === 'O') {
-  //             if (store.player === 'O' && response.game.cells[2] && response.game.cells[5] && response.game.cells[8] === 'O') {
-  //               if (store.player === 'O' && response.game.cells[0] && response.game.cells[4] && response.game.cells[8] === 'O') {
-  //                 if (store.player === 'O' && response.game.cells[2] && response.game.cells[4] && response.game.cells[6] === 'O') {
-  //                 }
-  //                 $('#message').text('Yay! O wins')
-  //                 // Here I need to freeze the board to no longer allow clicks
-  //                 $('.cell').off('click')
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  // add button appear here. where would function for next game appear?
+  if (store.player === 'X' && response.game.cells[0] && response.game.cells[1] && response.game.cells[2] === 'X') {
+    $('#message').text('Player X Wins!')
+    $('.cell').off('click')
+  } else {
+    console.log('still playing')
+  }
+  if (store.player === 'X' && response.game.cells[3] && response.game.cells[4] && response.game.cells[5] === 'X') {
+    $('#message').text('Player X Wins!')
+    $('.cell').off('click')
+  } else {
+    console.log('still playing')
+  }
+  if (store.player === 'X' && response.game.cells[6] && response.game.cells[7] && response.game.cells[8] === 'X') {
+    $('#message').text('Player X Wins!')
+    $('.cell').off('click')
+  } else {
+    console.log('no winner')
+    $('#start-new-game').show()
+}
 }
 
 const clickFailure = function () {
@@ -100,3 +77,46 @@ module.exports = {
   clickSuccess,
   clickFailure
 }
+
+// if (store.player === 'X') {
+//   if (store.player === 'X' && response.game.cells[0] && response.game.cells[1] && response.game.cells[2] === 'X') {
+//     if (store.player === 'X' && response.game.cells[3] && response.game.cells[4] && response.game.cells[5] === 'X') {
+//       if (store.player === 'X' && response.game.cells[6] && response.game.cells[7] && response.game.cells[8] === 'X') {
+//         if (store.player === 'X' && response.game.cells[0] && response.game.cells[3] && response.game.cells[6] === 'X') {
+//           if (store.player === 'X' && response.game.cells[1] && response.game.cells[4] && response.game.cells[7] === 'X') {
+//             if (store.player === 'X' && response.game.cells[2] && response.game.cells[5] && response.game.cells[8] === 'X') {
+//               if (store.player === 'X' && response.game.cells[0] && response.game.cells[4] && response.game.cells[8] === 'X') {
+//                 if (store.player === 'X' && response.game.cells[2] && response.game.cells[4] && response.game.cells[6] === 'X') {
+//                 }
+// $('#message').text('Yay! X wins')
+// // Here I need to freeze the board to no longer allow clicks
+// $('.cell').off('click')
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// } else if (store.player === 'O') {
+//   if (store.player === 'O' && response.game.cells[0] && response.game.cells[1] && response.game.cells[2] === 'O') {
+//     if (store.player === 'O' && response.game.cells[3] && response.game.cells[4] && response.game.cells[5] === 'O') {
+//       if (store.player === 'O' && response.game.cells[6] && response.game.cells[7] && response.game.cells[8] === 'O') {
+//         if (store.player === 'O' && response.game.cells[0] && response.game.cells[3] && response.game.cells[6] === 'O') {
+//           if (store.player === 'O' && response.game.cells[1] && response.game.cells[4] && response.game.cells[7] === 'O') {
+//             if (store.player === 'O' && response.game.cells[2] && response.game.cells[5] && response.game.cells[8] === 'O') {
+//               if (store.player === 'O' && response.game.cells[0] && response.game.cells[4] && response.game.cells[8] === 'O') {
+//                 if (store.player === 'O' && response.game.cells[2] && response.game.cells[4] && response.game.cells[6] === 'O') {
+//                 }
+//                 $('#message').text('Yay! O wins')
+//                 // Here I need to freeze the board to no longer allow clicks
+//                 $('.cell').off('click')
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
+// add button appear here. where would function for next game appear?
