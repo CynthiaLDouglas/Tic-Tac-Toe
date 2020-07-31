48eca7dc9e9f4a949ea6ec5a46fa1972cac9a34d
Cyn-D <cynthia.l.douglas@gmail.com>
2020-07-31 12:23:14 -0400

    I'm DONE!!!
    All bugs fixed. New branch to do final runthrough and merge/submit.

--- assets/scripts/games/ui.js
+++ assets/scripts/games/ui.js
@@ -3,13 +3,8 @@ store.counter = 0
 
 const newGameSuccess = function (response) {
   store.game = response.game
+  console.log('This is store.game before game over', store.game)
   store.player = 'X'
-  store.game.over = false
-  const numOfGame = (response) => {
-    store.counter += 1
-    return store.counter
-  }
-  numOfGame(response)
   $('.container').show()
   $('#start-game').hide()
   $('#start-new-game').show()
@@ -24,82 +19,84 @@ const newGameFailure = function () {
 }
 
 const clickSuccess = function (response) {
-  store.game = response.game
-  $('#start-new-game').show()
-  $(store.currentBox).text(store.player)
-  $('#message').text(`You played ${store.player}`)
-  const gameLogic = function (response) {
-    if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[1] && response.game.cells[1] === response.game.cells[2]) {
-      $('#message').text('X wins')
-      store.game.over = true
-    } else if (store.player === 'X' && response.game.cells[3] === 'X' && response.game.cells[3] === response.game.cells[4] && response.game.cells[4] === response.game.cells[5]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[6] === 'X' && response.game.cells[6] === response.game.cells[7] && response.game.cells[7] === response.game.cells[8]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[4] && response.game.cells[4] === response.game.cells[8]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[2] === 'X' && response.game.cells[2] === response.game.cells[4] && response.game.cells[4] === response.game.cells[6]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[3] && response.game.cells[3] === response.game.cells[6]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[1] === 'X' && response.game.cells[1] === response.game.cells[4] && response.game.cells[4] === response.game.cells[7]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else if (store.player === 'X' && response.game.cells[2] === 'X' && response.game.cells[2] === response.game.cells[5] && response.game.cells[5] === response.game.cells[8]) {
-      $('#message').text('X wins')
-      store.game.over = !store.game.over
-    } else {
-      if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[1] && response.game.cells[1] === response.game.cells[2]) {
-        $('#message').text('O wins')
+  if (store.game.over === false) {
+    store.game = response.game
+    $('#start-new-game').show()
+    $(store.currentBox).text(store.player)
+    $('#message').text(`You played ${store.player}`)
+    const gameLogic = function (response) {
+      if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[1] && response.game.cells[1] === response.game.cells[2]) {
+        $('#message').text('X wins')
+        store.game.over = true
+      } else if (store.player === 'X' && response.game.cells[3] === 'X' && response.game.cells[3] === response.game.cells[4] && response.game.cells[4] === response.game.cells[5]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[3] === 'O' && response.game.cells[3] === response.game.cells[4] && response.game.cells[4] === response.game.cells[5]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[6] === 'X' && response.game.cells[6] === response.game.cells[7] && response.game.cells[7] === response.game.cells[8]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[6] === 'O' && response.game.cells[6] === response.game.cells[7] && response.game.cells[7] === response.game.cells[8]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[4] && response.game.cells[4] === response.game.cells[8]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[4] && response.game.cells[4] === response.game.cells[8]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[2] === 'X' && response.game.cells[2] === response.game.cells[4] && response.game.cells[4] === response.game.cells[6]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[2] === 'O' && response.game.cells[2] === response.game.cells[4] && response.game.cells[4] === response.game.cells[6]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[0] === 'X' && response.game.cells[0] === response.game.cells[3] && response.game.cells[3] === response.game.cells[6]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[3] && response.game.cells[3] === response.game.cells[6]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[1] === 'X' && response.game.cells[1] === response.game.cells[4] && response.game.cells[4] === response.game.cells[7]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[1] === 'O' && response.game.cells[1] === response.game.cells[4] && response.game.cells[4] === response.game.cells[7]) {
-        $('#message').text('O wins')
-        store.game.over = !store.game.over
-      } else if (store.player === 'O' && response.game.cells[2] === 'O' && response.game.cells[2] === response.game.cells[5] && response.game.cells[5] === response.game.cells[8]) {
-        $('#message').text('O wins')
+      } else if (store.player === 'X' && response.game.cells[2] === 'X' && response.game.cells[2] === response.game.cells[5] && response.game.cells[5] === response.game.cells[8]) {
+        $('#message').text('X wins')
         store.game.over = !store.game.over
       } else {
-        if (store.game.cells.indexOf('') === -1) {
-          $('#message').text('It\'s a tie')
+        if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[1] && response.game.cells[1] === response.game.cells[2]) {
+          $('#message').text('O wins')
           store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[3] === 'O' && response.game.cells[3] === response.game.cells[4] && response.game.cells[4] === response.game.cells[5]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[6] === 'O' && response.game.cells[6] === response.game.cells[7] && response.game.cells[7] === response.game.cells[8]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[4] && response.game.cells[4] === response.game.cells[8]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[2] === 'O' && response.game.cells[2] === response.game.cells[4] && response.game.cells[4] === response.game.cells[6]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[0] === 'O' && response.game.cells[0] === response.game.cells[3] && response.game.cells[3] === response.game.cells[6]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[1] === 'O' && response.game.cells[1] === response.game.cells[4] && response.game.cells[4] === response.game.cells[7]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else if (store.player === 'O' && response.game.cells[2] === 'O' && response.game.cells[2] === response.game.cells[5] && response.game.cells[5] === response.game.cells[8]) {
+          $('#message').text('O wins')
+          store.game.over = !store.game.over
+        } else {
+          if (store.game.cells.indexOf('') === -1) {
+            $('#message').text('It\'s a tie')
+            store.game.over = !store.game.over
+          }
+        }
+        // if (store.game.over === true) {
+        //   $('.cell').off('click')
+        // }
+        if (store.player !== 'O') {
+          store.player = 'O'
+          return store.player
+        } else if (store.player === 'O') {
+          $(store.currentBox).text(store.player)
+          store.player = 'X'
+          return store.player
+        } else {
+          $('#message').text('Try another spot')
         }
       }
-      if (store.game.over === true) {
-        $('.cell').off('click')
-      }
-      if (store.player !== 'O') {
-        store.player = 'O'
-        return store.player
-      } else if (store.player === 'O') {
-        $(store.currentBox).text(store.player)
-        store.player = 'X'
-        return store.player
-      } else {
-        $('#message').text('Try another spot')
-      }
     }
+    gameLogic(response)
   }
-  gameLogic(response)
 }
 
 const clickFailure = function () {
